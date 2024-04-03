f32497d0ad729498ab877172873d8b294007cb59
sirln <siro@sirlawren.com>
2024-04-03 12:06:26 +0300

    Upated premierpick dashboards

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from app import app
+from datetime import datetime
 from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    current_time = datetime.now().strftime('%H:%M')
+    current_date = datetime.now().strftime('%d-%m')
+    current_year = datetime.now().strftime('%Y')
+    return render_template('index.html', now=current_time, today=current_date, year=current_year)
